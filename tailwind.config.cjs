3bb686f5470dcde29e4f08d01cfb0f39a75517c2
Anna Burdanova <burdanovaas@gmail.com>
2023-08-18 12:38:50 +0700

    feat: :lipstick: preload local fonts with tailwind and astro
    Guide for reference: https://brazy.one/blog/how-to-manage-and-preload-local-fonts-with-tailwind-in-astro/

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        signica: ["Signika Negative", "Open Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};

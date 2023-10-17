e9800a3ddb4713089863f711ee419b7c7776aa86
Anna Burdanova <burdanovaas@gmail.com>
2023-10-17 14:42:43 +0700

    refactor: :recycle: share smooth scroll instance across client side components

---
+++ src/scripts/smoothScroll.ts
@@ -0,0 +1,28 @@
+// @ts-ignore - no types for beta version of locomotive 5 scroll yet, ignore ts error
+import LocomotiveScroll from "locomotive-scroll";
+import isMobileDevice from "../scripts/isMobileDevice";
+
+let locomotiveScroll: any;
+
+const initSmoothScroll = () => {
+  // Skip if scroll is already instantiated
+  if (locomotiveScroll) return locomotiveScroll;
+
+  // Do not initialize smooth scroll on mobile devices (scroll is handled natively for better performance)
+  if (isMobileDevice()) return null;
+
+  const mainContentContainer = document.querySelector(
+    ".main-content"
+  ) as HTMLElement;
+
+  locomotiveScroll = new LocomotiveScroll({
+    lenisOptions: {
+      wrapper: mainContentContainer, // Specify which element is scrollable
+    },
+    // scrollCallback: onScroll, // Callback function on scroll event
+  });
+
+  return locomotiveScroll;
+};
+
+export default initSmoothScroll;

e77f95acf596b7fef54a0d9415f28988ef349a48
Anna Burdanova <burdanovaas@gmail.com>
2023-10-19 18:23:24 +0700

    build: :heavy_plus_sign: add support of react components and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
         applyBaseStyles: false,
       },
     }),
+    react(), // Enable React to support React JSX components.
   ],
 });
---
+++ src/components/sections/land-search/LandSearch.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+interface CounterProps {
+  initialValue?: number;
+}
+
+const Counter: React.FC<CounterProps> = ({ initialValue = 0 }) => {
+  const [count, setCount] = useState(initialValue);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;

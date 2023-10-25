dbe6350d054dfe4b2f260d3c1216a477bf69fa6b
Anna Burdanova <burdanovaas@gmail.com>
2023-10-25 20:32:20 +0700

    feat: :construction: implement sorting functionality

--- src/components/sections/land-search/SearchResults.tsx
+++ src/components/sections/land-search/SearchResults.tsx
@@ -1,20 +1,68 @@
+import { useState } from "react";
+
 import type { Land } from "../../../model/Land";
 import Card from "./card/Card";
+import Dropdown from "./Dropdown";
 
 interface Props {
   lands: Land[];
 }
 
+const SORT_OPTIONS = [
+  "Mới thêm trước",
+  "Rẻ nhất trước",
+  "Đắt nhất trước",
+  "Nhỏ nhất trước",
+  "Lớn nhất trước",
+];
+
+const sortLands = (lands: Land[], option: number) => {
+  if (option <= 0) return lands;
+
+  if (option === 1) return lands.toSorted((a, b) => a.price! - b.price!);
+
+  if (option === 2) return lands.toSorted((a, b) => b.price! - a.price!);
+
+  if (option === 3) return lands.toSorted((a, b) => a.area! - b.area!);
+
+  if (option === 4) return lands.toSorted((a, b) => b.area! - a.area!);
+
+  return lands;
+};
+
 const SearchResults: React.FC<Props> = ({ lands }) => {
+  const [sortOption, setSortOption] = useState(0);
+
+  const sortedLands = sortLands(lands, sortOption);
+
   if (lands.length === 0)
-    return <div>todo: text and btn link to reset filter</div>;
+    return <div>TODO: text and btn link to reset filter</div>;
 
   return (
-    <div>
-      {lands.map((land) => (
-        <Card key={land.slug} land={land} />
-      ))}
-    </div>
+    <>
+      <div className=" flex items-center justify-between mb-3  text-base font-normal">
+        <div>
+          <strong className="font-semibold">557 lo đất</strong> dang bán
+        </div>
+        <div className=" select-none text-sm">
+          Sắp xếp theo:{" "}
+          <Dropdown
+            options={SORT_OPTIONS}
+            selectedOption={sortOption}
+            onUpdateSelection={(option) => setSortOption(option)}
+          />
+        </div>
+      </div>
+      <div
+        className="h-full overflow-y-scroll  pr-3 pb-10"
+        id="scrollable-lands-container"
+        data-lenis-prevent
+      >
+        {sortedLands.map((land) => (
+          <Card key={land.slug} land={land} />
+        ))}
+      </div>
+    </>
   );
 };
 
